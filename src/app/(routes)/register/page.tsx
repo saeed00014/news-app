dc81ca88f5681cde52217dfbb755783a622ec40c
saeed <saeed00014@gmail.com>
2024-05-01 10:15:47 +0330

    fix form ui

--- src/app/(routes)/register/page.tsx
+++ src/app/(routes)/register/page.tsx
@@ -2,9 +2,11 @@ import Register from "./register";
 
 const Page = () => {
   return (
-    <section className="flex flex-col items-center md:justify-center justify-start lg:px-4 lg:py-0 p-2 w-full h-full max-w-[1400px] gap-4">
-      <div className="flex justify-center min-h-max w-full overflow-y-auto no-scrollbar">
-        <Register />
+    <section className="fixed right-0 left-0 lg:top-[81px] top-[65px] lg:bottom-0 bottom-[65px] flex justify-center items-center">
+      <div className="flex items-center h-full w-full overflow-y-auto no-scrollbar">
+        <div className="flex justify-center w-full my-auto">
+          <Register />
+        </div>
       </div>
     </section>
   );
